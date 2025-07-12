15d497a88d515464f56723d6d92b9bb494213b26
Your Name <contactrohanasif52@gmail.com>
2025-07-12 21:12:13 +0500

    added await in createdUser

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -52,7 +52,7 @@ const registerUser = asyncHandler( async (req, res)=>{
         })
 
         // remove password and refresh token field from response
-        const createdUser = User.findById(user.id).select(
+        const createdUser = await User.findById(user.id).select(
             "-password -refreshToken"
         )
         
