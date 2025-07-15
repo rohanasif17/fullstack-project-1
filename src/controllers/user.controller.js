fe0635a8db8b12b1b74692e23a5fcf6136a7b026
Your Name <contactrohanasif52@gmail.com>
2025-07-15 18:51:32 +0500

    Refactor changePassword to changeCurrentPassword and implement getCurrentUser endpoint for fetching user data

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -205,13 +205,31 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     }
 
 })
-const changePassword = asyncHandler(async(req, res) =>{
+const changeCurrentPassword = asyncHandler(async(req, res) =>{
+    const {oldPassword, newPassword} = req.body
 
+    const user = await User.findById(req.user?._id)
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+    if (!isPasswordCorrect) {
+        throw new ApiError(400, 'Invalid old Password')
+    }
+
+    user.password = newPassword
+    await user.save({validateBeforeSave: false})
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password changed successfully"))
+})
+const getCurrentUser = asyncHandler( async (req, res) =>{
+    res.status(200).json(new ApiResponse(200, req.user, 'current User data fetched successfully'))
 })
 export {
     registerUser,
     loginUser,
     logoutUser,
     refreshAccessToken,
-    changePassword
+    changeCurrentPassword,
+    getCurrentUser
 }
