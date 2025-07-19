491d7e6de3f7288b98f369b55e9440e8b716a94f
Your Name <contactrohanasif52@gmail.com>
2025-07-19 16:08:38 +0500

    Refactor user routes to enhance functionality by adding endpoints for changing password, updating avatar and cover image, retrieving user channel profile, and fetching watch history. Update existing routes for current user and account details.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import { registerUser, loginUser, logoutUser, refreshAccessToken, getCurrentUser, updateAccountDetails} from '../controllers/user.controller.js';
+import { registerUser, loginUser, logoutUser, refreshAccessToken, getCurrentUser, updateAccountDetails, changeCurrentPassword, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory} from '../controllers/user.controller.js';
 import {upload} from "../middlewares/multer.middleware.js"
 import {verifyJWT} from '../middlewares/auth.middleware.js'
 
@@ -20,23 +20,15 @@ router.route('/register').post(
 )
 router.route('/login').post(loginUser)
 
+//secured routes
 router.route('/logout').post(verifyJWT , logoutUser)
 router.route('/refresh-token').post(refreshAccessToken)
-router.route('/current-user').post(verifyJWT, getCurrentUser)
-router.route('/update-account').put(
-    verifyJWT,
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount: 1
-        },
-        {
-            name: "coverImage",
-            maxCount: 1
-        }
-    ]),
-    updateAccountDetails
-)
-
+router.route('/change-password').post(verifyJWT, changeCurrentPassword)
+router.route('/current-user').get(verifyJWT, getCurrentUser)
+router.route('/update-details').patch(verifyJWT, updateAccountDetails)
+router.route('/avatar-update').patch(verifyJWT, upload.single('avatar'), updateUserAvatar)
+router.route('/coverImage-update').patch(verifyJWT, upload.single('coverImage'), updateUserCoverImage)
+router.route('/c/:username').get(verifyJWT, getUserChannelProfile)
+router.route('/history').get(verifyJWT, getWatchHistory)
 
 export default router
